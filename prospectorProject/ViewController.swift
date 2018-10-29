bec7a7e0ccedc48e49f2c26b78c069413c5a58e9
period3 <period3@p-162mba-19.local>
2018-10-29 10:45:52 -0500

    collection view custom class

--- prospectorProject/ViewController.swift
+++ prospectorProject/ViewController.swift
@@ -7,12 +7,11 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
- 
+    
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        print("hello")
     }
 
     

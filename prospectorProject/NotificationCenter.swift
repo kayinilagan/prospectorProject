a42fcb4b1dc0851272c2da2cc9e937abe562742b
period3 <period3@p-162mba-19.d214.org>
2018-11-09 16:09:47 +0000

    Notification File up and running

---
+++ prospectorProject/NotificationCenter.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationCenter.swift
+//  prospectorProject
+//
+//  Created by period3 on 11/8/18.
+//  Copyright © 2018 period3. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationCenter: UNUserNotificationCenter {
+//    "aps": {
+//    "alert" : {
+//            "title" : "New Article",
+//            "subtile" : "This is created by Programmers",
+//            "body" : "This was cerated by the great group of programmers who are creating the app",
+//        },
+//        "badge" : 1
+//    },
+}

38a8611ac2b3b93593615da7baf840845653fe9e
period3 <period3@p-162mba-02.local>
2018-11-14 16:08:22 +0000

    f*** the segue my dudes

--- prospectorProject/ViewController.swift
+++ prospectorProject/ViewController.swift
@@ -114,11 +114,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
   
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let vc = segue.destination as! ArticleViewController
-        
-
-    }
+    
     
     
     

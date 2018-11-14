357bf0a88039e1fc2c1df486face5f7a62034460
period3 <period3@p-162mba-04.local>
2018-11-14 16:26:59 +0000

    Add 8 view controller files for the hamburger menu

---
+++ prospectorProject/TrendingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrendingViewController.swift
+//  prospectorProject
+//
+//  Created by period3 on 11/14/18.
+//  Copyright © 2018 period3. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

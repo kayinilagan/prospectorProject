9f995803a5f44c9e79fab49fea833313f05fb5c1
period3 <period3@p-162mba-04.local>
2018-11-15 16:51:07 +0000

    add pinterestLayout file

---
+++ prospectorProject/pinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  pinterestLayout.swift
+//  prospectorProject
+//
+//  Created by period3 on 11/15/18.
+//  Copyright © 2018 period3. All rights reserved.
+//
+
+import UIKit
+
+class pinterestLayout: UICollectionViewLayout {
+
+}

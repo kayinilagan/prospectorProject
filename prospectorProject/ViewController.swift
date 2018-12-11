f3fc721047b8002e60f807a576f0d21925e6f92a
period3 <period3@p-162mba-04.local>
2018-12-11 10:48:30 -0600

    changed title of navigation bar

--- prospectorProject/ViewController.swift
+++ prospectorProject/ViewController.swift
@@ -37,9 +37,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     {
         super.viewDidLoad()
         spinner.isHidden = true
-        let logo = UIImage(named: "prospectorLogol")
-        let imageView = UIImageView(image:logo)
-        self.navigationItem.titleView = imageView
 
         let homePageQuery = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fprospectornow.com%2F%3Ffeed%3Drss2"
 

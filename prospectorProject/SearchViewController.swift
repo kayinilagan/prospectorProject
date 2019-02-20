1c3d19e3944d5dce6f07e546795efdc5945f7435
period3 <period3@p-162mba-04.local>
2019-02-20 12:21:53 -0600

    Search View Controller is working, now to match articles

--- prospectorProject/SearchViewController.swift
+++ prospectorProject/SearchViewController.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class SearchViewController: ViewController {
+class SearchViewController: ViewController, UISearchBarDelegate {
+    
     
     @IBOutlet weak var searchCollectionView: UICollectionView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var data = [String]()
     
+    var filteredData = [String]().self
+    
     override func viewDidLoad() {
+        self.title = "PROSPECTORNOW"
         super.viewDidLoad()
-
+        searchCollectionView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
       
     }
     
-//    func updateSearchResults(for searchController: UISearchController) {
-//        nil
-//    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = searchCollectionView.dequeueReusableCell(withReuseIdentifier: "searchCell", for: indexPath) as! CollectionViewCell
+        cell.articleLabel.text = filteredData[indexPath.row]
+        return cell
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        searchCollectionView.reloadData()
+    }
 
 }

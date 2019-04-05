1efa670c9c4dc39c819e15692a83a4dac0f1c471
Me <period3@p-162mba-19.local>
2019-04-05 10:28:01 -0500

    sr

---
+++ ImageData.swift
@@ -0,0 +1,20 @@
+//
+//  ImageData.swift
+//  
+//
+//  Created by period3 on 4/5/19.
+//
+
+import Foundation
+
+class ImageData: Codable {
+    var image : Data
+    var like: Bool
+    var text: String
+    
+    init(image: Data, like: Bool, text: String) {
+        self.image = image
+        self.like = like
+        self.text = text
+    }
+}
